0126ac83a4c201701435fa256c08a79d9ba5bc6b
casianalex <alcaticsoft@gmail.com>
2015-07-13 13:25:54 +0300

    Initial commit

---
+++ app/src/main/java/vara/de/scoala/calculatorapostila/MainActivity.java
@@ -0,0 +1,53 @@
+package vara.de.scoala.calculatorapostila;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void CalculeazaTaxa(View view) {
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        String text = mySpinner.getSelectedItem().toString();
+
+        int numarDocumente = Integer.parseInt(text);
+
+        int cost = numarDocumente * 22 + 3;
+
+        TextView rezultat = (TextView)findViewById(R.id.textView3);
+        rezultat.setText(cost + "");
+    }
+}
